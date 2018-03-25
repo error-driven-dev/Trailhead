fcbb5406c0d69db1676c59699674cdc9cf161fa2
error-driven-dev <errordrivendev@gmail.com>
2018-03-25 02:38:19 -0700

    initial commit. set up api key, make API call and organize data

---
+++ Trailhead/Controllers/TrailController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Trailhead.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Trailhead.Controllers
+{
+    public class TrailController : Controller
+    {
+      
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://trailapi-trailapi.p.mashape.com");
+                client.DefaultRequestHeaders.Add("X-Mashape-Key", "K5jOkwvtBvmsh8TXTabBt7U9GG8Vp1PPeynjsnLhM43mHshsc7");
+                var response =
+                    await client.GetAsync(
+                        $"/?limit=25&q[activities_activity_type_name_eq]=hiking&q[state_cont]=washington&radius=25");
+                response.EnsureSuccessStatusCode();
+                var stringresult = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<Place>(stringresult);
+
+                return View(data);
+            }
+        }
+        public async Task<IActionResult> One()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://trailapi-trailapi.p.mashape.com");
+                client.DefaultRequestHeaders.Add("X-Mashape-Key", "K5jOkwvtBvmsh8TXTabBt7U9GG8Vp1PPeynjsnLhM43mHshsc7");
+                var response =
+                    await client.GetAsync(
+                        $"/?unique_id=25214");
+                response.EnsureSuccessStatusCode();
+                var stringresult = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<Place>(stringresult);
+
+                return View("Index", data);
+            }
+        }
+    }
+}
---
+++ Trailhead/Models/Trail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Trailhead.Models
+{
+    [JsonObject]
+    public class Trail
+    {
+        public string City { get; set; }
+        public string Name { get; set; }
+        
+    }
+
+    public class Place
+    {
+        public IEnumerable<JObject> Places { get; set; }
+    }
+}
